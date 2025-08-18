55ace87b7afdc4a8296f1092e7a91e2a80618147
Yashraj-CodeRed <yashrajmohite281103@gmail.com>
2025-08-19 01:30:46 +0530

    added class

--- class.cpp
+++ class.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+class New{
+
+    public:
+        int a;
+        int b;
+
+    int add(int a ,int b){
+        return a+b;
+    }
+};
 
 int main(){
     std::cout<< " hello git" << std::endl;
+
+    New a1;
+    a1.a = 10;
+    a1.b = 20;
+
+    int ans = a1.add(a1.a,a1.b);
+
+    cout << ans;
     return 0;
 }
